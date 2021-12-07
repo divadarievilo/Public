fdbe93664e6afc2c3f831c8aa47c7edd14c83d06
divadarievilo <91880904+divadarievilo@users.noreply.github.com>
2021-12-07 12:19:51 +0000

    Create day1_part1.py

---
+++ Advent-Of-Code/2021/day1/day1_part1.py
@@ -0,0 +1,20 @@
+# Open/Close TXT file (read mode) to get the input data
+with open('inputs.txt', "r") as inputs:
+    # read file inputs.txt
+    data = [int(linha) for linha in inputs.read().split("\n")]
+
+# Set variables for counters
+count_inc = 0
+count_desc = 0
+
+# Loop to count the number of increases/decreases while comparing "Number" with "Number - 1"
+for i in range(1, len(data)):
+    if data[i] > data[i-1]:
+        count_inc += 1
+    else:
+        count_desc += 1
+
+# print number of increases, decreases and the sum of both
+print(f"Increase: {str(count_inc)}")
+print(f"Decrease: {str(count_desc)}")
+print(f"Sum: {str(count_inc+count_desc)}")
