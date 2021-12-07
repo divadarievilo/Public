516935d27c3a92e39033da3cbb6234e32906964d
divadarievilo <91880904+divadarievilo@users.noreply.github.com>
2021-12-07 12:25:23 +0000

    Create day2_part2.py

---
+++ Advent-Of-Code/2021/day2/day2_part2.py
@@ -0,0 +1,25 @@
+# Open/Close TXT file (read mode) to get the input data
+with open('inputs.txt', "r") as inputs:
+    # read file inputs.txt
+    data = [linha for linha in inputs.read().split("\n")]
+
+#initialize variables
+horizontal_position = 0
+depth_position = 0
+aim_position = 0
+
+# loop with enumerate to get the index and the current element of data.
+for index, i in enumerate(data):
+    if "forward" in i:
+        horizontal_position += int(i[- 1])
+        depth_position += aim_position * int(i[- 1])
+    elif "up" in i:
+        aim_position -= int(i[- 1])
+    elif "down" in i:
+        aim_position += int(i[- 1])
+
+# Print results
+print(f"Horizontal Position: {horizontal_position}")
+print(f"Depth Position: {depth_position}")
+print(f"Aim Position: {aim_position}")
+print(f"'Horizontal' multiplied by 'Depth' positions: {horizontal_position * depth_position}")
