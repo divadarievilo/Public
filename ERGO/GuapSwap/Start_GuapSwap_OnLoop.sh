c2bb93517e8439334ff85606d821de5c641e5282
divadarievilo <91880904+divadarievilo@users.noreply.github.com>
2022-03-17 18:14:31 +0000

    Update Start_GuapSwap_OnLoop.sh

--- ERGO/GuapSwap/Start_GuapSwap_OnLoop.sh
+++ ERGO/GuapSwap/Start_GuapSwap_OnLoop.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-# Start GuapSwap (1h loop) with a new Tmux Session
+# Start GuapSwap with a new Tmux Session
+# NOTE: Change GuapSwap installation folder
+
 tmux new-session -d -s GuapSwap
 tmux send-keys -t GuapSwap:0 "cd ~/guapSwap/guapswap-1.0.1-beta ; java -jar guapswap-1.0.1-beta.jar swap <YOUR GENERATED ERGO PROXY ADDRESS>" Enter
 tmux a
